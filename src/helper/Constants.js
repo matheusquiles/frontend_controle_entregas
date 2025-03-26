4ad7c4935052c35454be6ed6d99120964037fc74
Matheus <matheussquiles@gmail.com>
2025-03-26 19:40:37 -0300

    adicionando url da api 2

--- src/helper/Constants.js
+++ src/helper/Constants.js
@@ -1,5 +1,5 @@
 //apiConstants.js
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://api-al2a.onrender.com/api';
 
 
 export const API_BASE = `${API_BASE_URL}`;
